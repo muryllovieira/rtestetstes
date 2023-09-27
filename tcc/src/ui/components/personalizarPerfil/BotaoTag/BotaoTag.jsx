7037e37ff85378f2e4798c1fa443236587581494
MATEUS ALVES DA SILVA <22282205@dsn127m1249388.cfp127aluno.local>
2023-09-27 15:35:27 -0300

    Componentes Iniciados v2.12 - Tela PersonalizarTags

--- tcc/src/ui/components/personalizarPerfil/BotaoTag/BotaoTag.jsx
+++ tcc/src/ui/components/personalizarPerfil/BotaoTag/BotaoTag.jsx
@@ -1,8 +1,19 @@
 import React from 'react'
+import "./styleBotaoTag.css"
+import { useState} from 'react'
 
-function BotaoTag() {
-  return (
-    <div>BotaoTag</div>
+function BotaoTag({text, option, setValue}) {
+
+  const [tagAtiva, setTagAtiva] = useState(false)
+
+  return(
+      <>
+          <div onClick={() => {
+              setTagAtiva(!tagAtiva)
+          }} className={`botaoTag ${tagAtiva ? "botaoTagAtivo" : ""}`}>
+              <p>{text}</p>
+          </div>
+      </>
   )
 }
 

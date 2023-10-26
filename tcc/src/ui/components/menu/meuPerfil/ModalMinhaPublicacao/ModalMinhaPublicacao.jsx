0c26c34c200957521cae165ed22ebd36a54c04e4
MARCELO GABRIEL VIEIRA SANTIAGO <22282204@dsn127m1249386.cfp127aluno.local>
2023-10-26 16:56:41 -0300

    Componentes Iniciados v2.29 - Modal Publicação do Perfil

--- tcc/src/ui/components/menu/meuPerfil/ModalMinhaPublicacao/ModalMinhaPublicacao.jsx
+++ tcc/src/ui/components/menu/meuPerfil/ModalMinhaPublicacao/ModalMinhaPublicacao.jsx
@@ -1,8 +1,9 @@
 import React from 'react'
 import './styleModalMinhaPublicacao.css'
 import './product-image-slider.scss'
-import { Swiper, SwiperSlide } from 'swiper/react'
-import {register} from 'swiper/element/bundle'
+import { Swiper, SwiperSlide, useSwiper } from 'swiper/react'
+import { Controller, Virtual } from 'swiper/modules'
+import { register } from 'swiper/element/bundle'
 import { useNavigate } from 'react-router-dom'
 import InputGlobal from '../../../global/InputGlobal/InputGlobal'
 import BotaoAncoraGlobal from '../../../global/BotaoAncoraGlobal/BotaoAncoraGlobal'
@@ -10,159 +11,213 @@ import ImagemPerfil from './images/imagemPerfil.png'
 import IconeMais from './images/icone_mais.svg'
 import Enviar from './images/enviar.svg'
 import Fechar from './images/fechar.svg'
+import { useState } from 'react'
 
 register()
 
 import 'swiper/css'
 import 'swiper/css/navigation'
 import 'swiper/css/scrollbar'
+import { useEffect } from 'react'
 
-const ModalMinhaPublicacao = ({isOpen}) => {
 
-    const navigate = useNavigate()
+const ModalMinhaPublicacao = ({ isOpen }) => {
 
-    const data = [
-        {id: '1', image: 'https://www.curitiba.pr.leg.br/atividade-parlamentar/legislacao/imagens/bandeira-do-brasil.png/image'},
-        {id: '2', image: 'https://imagepng.org/wp-content/uploads/2017/10/batman-simbolo.png'},
-        {id: '3', image: 'https://s2-techtudo.glbimg.com/CDCDKUhS0FMmWH6daMavnixT6cg=/0x0:1024x609/984x0/smart/filters:strip_icc()/i.s3.glbimg.com/v1/AUTH_08fbf48bc0524877943fe86e43087e7a/internal_photos/bs/2022/c/u/15eppqSmeTdHkoAKM0Uw/dall-e-2.jpg'},
-        {id: '4', image: 'https://t.ctcdn.com.br/JlHwiRHyv0mTD7GfRkIlgO6eQX8=/640x360/smart/i257652.jpeg'},
-        {id: '4', image: 'https://t.ctcdn.com.br/JlHwiRHyv0mTD7GfRkIlgO6eQX8=/640x360/smart/i257652.jpeg'},
-        {id: '4', image: 'https://t.ctcdn.com.br/JlHwiRHyv0mTD7GfRkIlgO6eQX8=/640x360/smart/i257652.jpeg'},
-        {id: '4', image: 'https://t.ctcdn.com.br/JlHwiRHyv0mTD7GfRkIlgO6eQX8=/640x360/smart/i257652.jpeg'},
-        {id: '4', image: 'https://t.ctcdn.com.br/JlHwiRHyv0mTD7GfRkIlgO6eQX8=/640x360/smart/i257652.jpeg'},
-        {id: '4', image: 'https://t.ctcdn.com.br/JlHwiRHyv0mTD7GfRkIlgO6eQX8=/640x360/smart/i257652.jpeg'},
+  const navigate = useNavigate()
 
-      ]
+  const swiper = useSwiper()
 
-if (isOpen) {
-    return(
-        <>
-<div className='modal__background'>
-  <div className='formularioMinhaPublicacao'>
+  const [firstSwiper, setFirstSwiper] = useState(null);
+  const [secondSwiper, setSecondSwiper] = useState(null);
 
-    <div className='setor_01'>
+  useEffect(() => {
+    console.log(firstSwiper)
+  }, [firstSwiper])
 
-      <div className='setor01_header'>
-        <img src={Fechar} alt="Voltar" className='setaVoltar' onClick={() => {
-          navigate(-1)
-        }}/>
-      </div>
-    
-      <div className='setor01_main'>
-          <Swiper
-            slidesPerView={1}
-            navigation
-          >
-            {data.map((item) => (
-              <SwiperSlide key={item.id} className='slide_post'>
-                <img src={item.image} alt="" className='slide_item'/>
-              </SwiperSlide>
-            ))}
-          </Swiper>
-      </div>
+  const [opcoes, setOpcoes] = useState(false)
+  const [value, setValue] = useState(0)
 
-    <div className='lista_imagens'>
-          <Swiper
-            slidesPerView={4}
-          >
-            {data.map((item) => (
-              <SwiperSlide key={item.id} >
-                <img src={item.image} alt="" className='thumb_item'/>
-              </SwiperSlide>
-            ))}
-          </Swiper>
-      </div>
-     
-      <div className='setor01_footer'>
-        <BotaoAncoraGlobal
-          titulo={'Dar Ponto'}
-        ></BotaoAncoraGlobal>
+  const setListLenght = () => {
 
-        <BotaoAncoraGlobal
-          titulo={'Responder'}
-        ></BotaoAncoraGlobal>
-      </div>
-  </div>
+    if (data.length >= 4) {
+      setValue(4)
+    } else {
+      setValue(data.length)
+    }
+  }
 
-  <div className='setor_02'>
-    
-    <div className="setor02_header">
-      <div className="card">
+  const data = [
+    { id: '1', image: 'https://www.curitiba.pr.leg.br/atividade-parlamentar/legislacao/imagens/bandeira-do-brasil.png/image' },
+    { id: '2', image: 'https://imagepng.org/wp-content/uploads/2017/10/batman-simbolo.png' },
+    { id: '3', image: 'https://s2-techtudo.glbimg.com/CDCDKUhS0FMmWH6daMavnixT6cg=/0x0:1024x609/984x0/smart/filters:strip_icc()/i.s3.glbimg.com/v1/AUTH_08fbf48bc0524877943fe86e43087e7a/internal_photos/bs/2022/c/u/15eppqSmeTdHkoAKM0Uw/dall-e-2.jpg' },
+    { id: '4', image: 'https://t.ctcdn.com.br/JlHwiRHyv0mTD7GfRkIlgO6eQX8=/640x360/smart/i257652.jpeg' },
+    { id: '4', image: 'https://t.ctcdn.com.br/JlHwiRHyv0mTD7GfRkIlgO6eQX8=/640x360/smart/i257652.jpeg' },
+    { id: '4', image: 'https://t.ctcdn.com.br/JlHwiRHyv0mTD7GfRkIlgO6eQX8=/640x360/smart/i257652.jpeg' },
+    { id: '4', image: 'https://t.ctcdn.com.br/JlHwiRHyv0mTD7GfRkIlgO6eQX8=/640x360/smart/i257652.jpeg' },
+  ]
 
-        <div className='perfil_container'>
-          <img src={ImagemPerfil} className='foto_perfil'/>
-          <p className='nome_perfil'>Beltrana dos santos</p>
-        </div>
+  useEffect(() => {
+    setListLenght()
+  }, [value])
 
-        <div className='menu'>
-          <img src={IconeMais} className='botao_mais' />
-        </div>
+  if (isOpen) {
+    return (
+      <>
+        <div className='modal__background'>
+          <div className='formularioMinhaPublicacao'>
 
-    </div>
-  </div>
+            <div className='setor_01'>
 
-  <div className='setor02_main'>
+              <div className='setor01_header'>
+                <img src={Fechar} alt="Voltar" className='setaVoltar' onClick={() => {
+                  navigate(-1)
+                }} />
+              </div>
 
-    <div className='titulo_descricao'>
-        <h1 className='titulo'>Titulo</h1>
-        <p className='descricao'>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Modi, architecto iure? Hic iste iusto maiores accusantium esse ipsa, molestiae libero blanditiis! Consectetur voluptatibus excepturi accusantium odio ducimus laborum assumenda dignissimos.</p>
-    </div>
+              <div className='setor01_main'>
+                <Swiper
+                  slidesPerView={1}
+                  navigation
+                  virtual
+                  modules={[Controller, Virtual]}
+                  onSwiper={setFirstSwiper}
+                  controller={{ control: secondSwiper }}
+                >
+                  {data.map((item, index) => (
+                    <SwiperSlide
+                      key={item}
+                      className='slide_post'
+                      virtualIndex={index}
 
-    <div className='lista_comentarios'>
+                    >
+                      <img src={item.image} alt="" className='slide_item' />
+                    </SwiperSlide>
+                  ))}
+                </Swiper>
+              </div>
 
-        <div className='card_comentario'>
-          <img src={ImagemPerfil} alt="" className='foto_perfil'/>
-          <div className='container_textos'>
-            <p className='nome'>Mayara</p>
-            <p className='texto_comentario'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Et magni neque beatae magnam, quia ab labore nihil itaque repellendus aut cum reprehenderit distinctio cupiditate velit voluptate dolor exercitationem cumque aliquid.</p>
-            <p className='responder'>Responder</p>
+              <div className='lista_imagens'>
+                <Swiper
+                  slidesPerView={value}
+                  modules={[Controller, Virtual]}
+                  onSwiper={setSecondSwiper}
+                  controller={{ control: firstSwiper }}
+                  virtual
+                >
+                  {data.map((item, index) => (
+                    <SwiperSlide
+                      key={item}
+                      virtualIndex={index}
+                    
+
+                    >
+                      <img src={item.image} alt="" className='thumb_item' />
+                    </SwiperSlide>
+                  ))}
+                </Swiper>
+              </div>
+
+              <div className='setor01_footer'>
+                <BotaoAncoraGlobal
+                  titulo={'Dar Ponto'}
+                ></BotaoAncoraGlobal>
+
+                <BotaoAncoraGlobal
+                  titulo={'Responder'}
+                ></BotaoAncoraGlobal>
+              </div>
+            </div>
+
+            <div className='setor_02'>
+
+              <div className="setor02_header">
+                <div className="card">
+
+                  <div className='perfil_container'>
+                    <img src={ImagemPerfil} className='foto_perfil' />
+                    <p className='nome_perfil'>Beltrana dos santos</p>
+                  </div>
+
+                  <div className='menu'>
+                    <img onClick={() => {
+                      setOpcoes(!opcoes)
+                    }} src={IconeMais} className='botao_mais' />
+                    {
+                      opcoes == false ? (
+                        null
+                      ) : (
+                        <p className='teste'>
+                          asoidjaij
+                        </p>
+                      )
+                    }
+                  </div>
+
+                </div>
+              </div>
+
+              <div className='setor02_main'>
+
+                <div className='titulo_descricao'>
+                  <h1 className='titulo'>Titulo</h1>
+                  <p className='descricao'>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Modi, architecto iure? Hic iste iusto maiores accusantium esse ipsa, molestiae libero blanditiis! Consectetur voluptatibus excepturi accusantium odio ducimus laborum assumenda dignissimos.</p>
+                </div>
+
+                <div className='lista_comentarios'>
+
+                  <div className='card_comentario'>
+                    <img src={ImagemPerfil} alt="" className='foto_perfil' />
+                    <div className='container_textos'>
+                      <p className='nome'>Mayara</p>
+                      <p className='texto_comentario'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Et magni neque beatae magnam, quia ab labore nihil itaque repellendus aut cum reprehenderit distinctio cupiditate velit voluptate dolor exercitationem cumque aliquid.</p>
+                      <p className='responder'>Responder</p>
+                    </div>
+                  </div>
+                  <div className='card_comentario'>
+                    <img src={ImagemPerfil} alt="" className='foto_perfil' />
+                    <div className='container_textos'>
+                      <p className='nome'>Mayara</p>
+                      <p className='texto_comentario'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Et magni neque beatae magnam, quia ab labore nihil itaque repellendus aut cum reprehenderit distinctio cupiditate velit voluptate dolor exercitationem cumque aliquid.</p>
+                      <p className='responder'>Responder</p>
+                    </div>
+                  </div>
+                  <div className='card_comentario'>
+                    <img src={ImagemPerfil} alt="" className='foto_perfil' />
+                    <div className='container_textos'>
+                      <p className='nome'>Mayara</p>
+                      <p className='texto_comentario'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Et magni neque beatae magnam, quia ab labore nihil itaque repellendus aut cum reprehenderit distinctio cupiditate velit voluptate dolor exercitationem cumque aliquid.</p>
+                      <p className='responder'>Responder</p>
+                    </div>
+                  </div>
+                  <div className='card_comentario'>
+                    <img src={ImagemPerfil} alt="" className='foto_perfil' />
+                    <div className='container_textos'>
+                      <p className='nome'>Mayara</p>
+                      <p className='texto_comentario'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Et magni neque beatae magnam, quia ab labore nihil itaque repellendus aut cum reprehenderit distinctio cupiditate velit voluptate dolor exercitationem cumque aliquid.</p>
+                      <p className='responder'>Responder</p>
+                    </div>
+                  </div>
+
+                </div>
+              </div>
+
+              <div className='setor02_footer'>
+                <InputGlobal
+                  type={'email'}
+                  emailWeb={true}
+                  placeholder={'Escreva um comentário...'}
+                ></InputGlobal>
+                <img src={Enviar} alt="" />
+              </div>
+
+            </div>
           </div>
         </div>
-        <div className='card_comentario'>
-          <img src={ImagemPerfil} alt="" className='foto_perfil'/>
-          <div className='container_textos'>
-            <p className='nome'>Mayara</p>
-            <p className='texto_comentario'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Et magni neque beatae magnam, quia ab labore nihil itaque repellendus aut cum reprehenderit distinctio cupiditate velit voluptate dolor exercitationem cumque aliquid.</p>
-            <p className='responder'>Responder</p>
-          </div>
-        </div>
-        <div className='card_comentario'>
-          <img src={ImagemPerfil} alt="" className='foto_perfil'/>
-          <div className='container_textos'>
-            <p className='nome'>Mayara</p>
-            <p className='texto_comentario'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Et magni neque beatae magnam, quia ab labore nihil itaque repellendus aut cum reprehenderit distinctio cupiditate velit voluptate dolor exercitationem cumque aliquid.</p>
-            <p className='responder'>Responder</p>
-          </div>
-        </div>
-        <div className='card_comentario'>
-          <img src={ImagemPerfil} alt="" className='foto_perfil'/>
-          <div className='container_textos'>
-            <p className='nome'>Mayara</p>
-            <p className='texto_comentario'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Et magni neque beatae magnam, quia ab labore nihil itaque repellendus aut cum reprehenderit distinctio cupiditate velit voluptate dolor exercitationem cumque aliquid.</p>
-            <p className='responder'>Responder</p>
-          </div>
-        </div>
-
-    </div>
-  </div>  
-
-      <div className='setor02_footer'>
-        <InputGlobal
-          type={'email'}
-          emailWeb={true}
-          placeholder={'Escreva um comentário...'}
-        ></InputGlobal>
-        <img src={Enviar} alt="" />
-      </div>
-  
-    </div>
-  </div>
-</div>
- </>
+      </>
     )
-} else {
+  } else {
     null
-}
+  }
 }
 
 export default ModalMinhaPublicacao

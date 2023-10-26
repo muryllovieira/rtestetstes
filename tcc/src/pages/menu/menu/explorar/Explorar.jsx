aa47f5aff4457d975fcfebab0852d5086d2978a1
MATEUS ALVES DA SILVA <22282205@dsn127m1249388.cfp127aluno.local>
2023-10-26 16:52:49 -0300

    Integracao Iniciada - PublicacoesMeuPerfil

--- tcc/src/pages/menu/menu/explorar/Explorar.jsx
+++ tcc/src/pages/menu/menu/explorar/Explorar.jsx
@@ -1,9 +1,35 @@
-import React from 'react'
+import React, { useEffect, useState, useContext } from 'react'
 import "./styleExplorar.css"
 import { Outlet } from 'react-router-dom'
-import FotoPerfil from './images/imagemPerfil.png'
+import blogFetch from '../../../../data/services/api/ApiService'
+import UserContext from '../../../../data/hooks/context/UserContext'
 
 function Explorar() {
+
+  const {accessToken} = useContext(UserContext)
+
+  const [listaPublicacoes, setListaPublicacoes] = useState()
+
+  useEffect(() => {
+    getPublicacoes()
+  }, [])
+
+  const getPublicacoes = async () => {
+    try {
+      const response = await blogFetch.get('/publicacao/select_all', {
+        headers: {
+          'x-access-token' : accessToken
+        }
+      })
+
+      setListaPublicacoes(response.data)
+      console.log(response)
+
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
 
   return (
     <>
@@ -23,20 +49,33 @@ function Explorar() {
         <section className='containerExplorar__secaoPublicacao'>
 
           <div className='secaoPublicacao__filtroPublicacao'>
-            <span className='maisPopulares'>MAIS POPULARES <i></i> </span>
-            <span className='recentes'>RECENTES <i></i> </span>
+            <span className='maisPopulares' activeClassName='selected'>MAIS POPULARES <i></i> </span>
+            <span className='recentes' activeClassName='selected'>RECENTES <i></i> </span>
             
           </div>
 
           <div className='secaoPublicacao__listaPublicacoes'>
 
-            <div className="cardPublicacao">
-              <img src={FotoPerfil} className='fotoPublicacao' alt="" />
-              <div className='container_textos'>
-                <p className='nomePerfilPublicacao'>Mayara</p>
-                <p className='tituloPublicacao'>Título</p>
-              </div> 
-            </div>
+
+            {
+              listaPublicacoes === undefined ?(
+                <p>Carregando...</p>
+              ) : (
+                listaPublicacoes.publicacoes.map((data) => (
+                  data.anexos.map((item) => (
+                    <div className="cardPublicacao" key={data.id}>
+                    <img src={item.anexo} className='fotoPublicacao' alt="" />
+                  <div className='container_textos'>
+                    <p className='nomePublicacao'>{data.titulo}</p>
+                    <p className='descricaoPublicacao'>{data.descricao} <p>...</p> </p>
+                  </div> 
+                </div>
+                  
+                  ))
+                ))
+              )
+            }
+
             
           </div>
 
---
+++ tcc/src/ui/components/menu/meuPerfil/ModalMinhaPublicacao/modalMenu/ModalMenu.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import './styleModalMenu.css'
+const ModalMenu = ({isOpen}) => {
+  return (
+    <>
+            <div className='modal_container'>
+                <button className='opcoes'></button>
+                <button className='opcoes'></button>
+            </div>
+    </>
+  )
+}
+
+export default ModalMenu

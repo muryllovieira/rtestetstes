397eae1d8b61c5d161a0bb0d8d38d0ffbddd73cb
MATEUS ALVES DA SILVA <22282205@dsn127m1249388.cfp127aluno.local>
2023-10-30 16:51:47 -0300

    Componentes Iniciados v2.31 - Configuracoes

--- tcc/src/pages/menu/menu/configuracoes/Configuracoes.jsx
+++ tcc/src/pages/menu/menu/configuracoes/Configuracoes.jsx
@@ -1,8 +1,35 @@
 import React from 'react'
+import { Outlet } from 'react-router-dom'
+import './styleConfiguracoes.css'
+import setaDireita from './images/setaDireita.png'
 
 function Configuracoes() {
   return (
-    <div>Configuracoes</div>
+    <>
+      <Outlet/>
+      <div className='containerConfiguracoes'>
+
+        <div className='containerExplorar__tituloEFoto'>
+            <p>
+              Configuracoes
+            </p>
+          <div className='tituloEFoto__cardFoto'></div>
+        </div>
+
+      <div className='containerConfiguracoes_main'>
+        <div className="botoesConfiguracao">
+          <h1 className='titulo'>Aparência</h1>
+        </div>
+        <div className="botoesConfiguracao"><h1 className='titulo'>Aparência</h1></div>
+        <div className="botoesConfiguracao"><h1 className='titulo'>Aparência</h1></div>
+        <div className="botoesConfiguracao"><h1 className='titulo'>Aparência</h1></div>
+        <div className="botoesConfiguracao"><h1 className='titulo'>Aparência</h1></div>
+      </div>
+
+      </div>
+
+      <div className='containerConfiguracoes_footer'></div>
+    </>
   )
 }
 
